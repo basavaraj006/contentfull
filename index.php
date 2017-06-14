2737d67428bbb744777aa590fa123a6377b97048
basavaraj <basavaraj.havaler@mindtree.com>
2017-06-14 12:46:34 +0530

    secondt commit

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 //echo "Hello"; exit;
 include 'header.php';
-include 'slider.php';
-include 'view.php';
+//include 'slider.php';
+//include 'view.php';
+use Contentful\Delivery\Client;
+require_once __DIR__ . '/vendor/autoload.php';
+$spaceID = 'wjaul0uqh9mi';
+$accessToken = '87f82c16ddb35bad4d641a7c7f7fda5d8e658a398cc8868cab32742b25918e67';
+$client = new Client($accessToken, $spaceID);
+$entries = $client->getEntries();
+
+print_r($entries);
 include 'footer.php';
 ?>
