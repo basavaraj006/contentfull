b27cf6e2cf353559307b31b2660e43299c11cf9b
basavaraj <basavaraj.havaler@mindtree.com>
2017-06-14 12:48:32 +0530

    secondt commit

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 //echo "Hello"; exit;
-include 'header.php';
+//include 'header.php';
 //include 'slider.php';
 //include 'view.php';
 use Contentful\Delivery\Client;
@@ -9,7 +9,7 @@ $spaceID = 'wjaul0uqh9mi';
 $accessToken = '87f82c16ddb35bad4d641a7c7f7fda5d8e658a398cc8868cab32742b25918e67';
 $client = new Client($accessToken, $spaceID);
 $entries = $client->getEntries();
-
+echo "hello";
 print_r($entries);
-include 'footer.php';
+//include 'footer.php';
 ?>
